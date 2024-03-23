68f7b2c091d51d0ce05d52c1776894dd1c66ccad
= <chauhanpoojaa44@gmail.com>
2024-03-23 14:35:20 +0530

    Updates

--- src/components/brand/import.js
+++
@@ -1,5 +0,0 @@
-export {default as google} from '../../assets/google.png'
-export {default as slack} from '../../assets/slack.png'
-export {default as atlassian} from '../../assets/atlassian.png'
-export {default as dropbox} from '../../assets/dropbox.png'
-export {default as shopify} from '../../assets/shopify.png'
